9219a932c47b9e97e685fa9fd6c96c9d49eab11c
nspeer12 <nspeer252@gmail.com>
2023-06-29 17:25:30 -0400

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+const sections = ["intro", "work", "projects", "interests"];
+
+sections.forEach((section) => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth / 2, window.innerHeight);
+  
+  document.getElementById(`${section}-canvas`).appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  animate();
+});
