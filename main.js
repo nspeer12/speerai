df092b0d8f7abaae3d2bb34150bab52b89ebca33
nspeer12 <nspeer252@gmail.com>
2023-07-01 21:50:52 -0400

    looking good

--- main.js
+++ main.js
@@ -3,70 +3,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { renderTorus } from './src/torus.js';
 import { nn } from './src/nn';
-import { renderTheS } from './src/thes';
+import { renderThes } from './src/thes';
 import { spot } from './src/spot';
 
-function createSceneAndCamera() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
-  return { scene, camera };
-}
-
-function addLightingToScene(scene) {
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
-  const pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(5, 5, 5);
-  scene.add(pointLight);
-}
-
-function createRenderer() {
-  // Get the parent container
-  var parentContainer = document.getElementById('intro-canvas');
-
-  // Get width and height of the parent container
-  var width = parentContainer.offsetWidth;
-  var height = parentContainer.offsetHeight;
-
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  parentContainer.appendChild(renderer.domElement);
-  return renderer;
-}
-
-function createControls(camera, renderer) {
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.update();
-  return controls;
-}
-
-function animate(controls, renderer, scene, camera) {
-  function frame() {
-    requestAnimationFrame(frame);
-    controls.update();
-    renderer.render(scene, camera);
-  }
-  frame();
-}
-
-function renderIntro() {
-  const { scene, camera } = createSceneAndCamera();
-  addLightingToScene(scene);
-  const renderer = createRenderer('intro');
-  const textureLoader = new THREE.TextureLoader();
-  const texture = textureLoader.load('img/a.png');
-  const material = new THREE.MeshPhongMaterial({ map: texture });
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const mesh = new THREE.Mesh(cubeGeometry, material);
-  scene.add(mesh);
-  const controls = createControls(camera, renderer);
-  animate(controls, renderer, scene, camera);
-}
 
 
-// // call each function
-renderIntro();
 nn();
 renderTorus();
 spot();
+renderThes();
--- src/thes.js
+++ src/thes.js
@@ -1,10 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+let clock = new THREE.Clock();
+
 function createSceneAndCamera() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 10;
+    camera.position.z = 12;
+    camera.position.y = 12;
+    camera.position.x = 12;
     return { scene, camera };
 }
 
@@ -19,70 +23,41 @@ function addLightingToScene(scene) {
 function createRenderer(section) {
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth / 2, window.innerHeight);
-    document.getElementById(`${section}-canvas`).appendChild(renderer.domElement);
+    document.getElementById(`thes-canvas`).appendChild(renderer.domElement);
     return renderer;
 }
 
-export function renderTheS() {
+export function renderThes() {
     const { scene, camera } = createSceneAndCamera();
     addLightingToScene(scene);
     const renderer = createRenderer('thes');
 
     const material = new THREE.LineBasicMaterial({ color: 0xffffff });
 
-    // Set the size of the S
-    const size = 3;
+    fetch('./src/thes.json')
+        .then(response => response.json())
+        .then(data => {
+            const vertices = data.vertices.map(v => new THREE.Vector3(v.x, v.y, v.z));
+            const edges = data.edges.map(([start, end]) => [vertices[start], vertices[end]]);
 
-    // Define the points for the "Cool S" shape
-    const points1 = [
-        new THREE.Vector3(-size, size, 0),
-        new THREE.Vector3(0, size * 2, 0),
-        new THREE.Vector3(size, size, 0),
-    ];
-    const points2 = [
-        new THREE.Vector3(-size, -size, 0),
-        new THREE.Vector3(0, -size * 2, 0),
-        new THREE.Vector3(size, -size, 0),
-    ];
-    const points3 = [
-        new THREE.Vector3(-size, size, 0),
-        new THREE.Vector3(-size, -size, 0),
-    ];
-    const points4 = [
-        new THREE.Vector3(size, size, 0),
-        new THREE.Vector3(size, -size, 0),
-    ];
-    const points5 = [
-        new THREE.Vector3(0, size, 0),
-        new THREE.Vector3(0, 0, 0),
-    ];
-    const points6 = [
-        new THREE.Vector3(0, -size, 0),
-        new THREE.Vector3(0, 0, 0),
-    ];
+            edges.forEach(([start, end]) => {
+                const points = [start, end];
+                const geometry = new THREE.BufferGeometry().setFromPoints(points);
+                const line = new THREE.Line(geometry, material);
+                scene.add(line);
+            });
 
-    const geometry1 = new THREE.BufferGeometry().setFromPoints(points1);
-    const geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
-    const geometry3 = new THREE.BufferGeometry().setFromPoints(points3);
-    const geometry4 = new THREE.BufferGeometry().setFromPoints(points4);
-    const geometry5 = new THREE.BufferGeometry().setFromPoints(points5);
-    const geometry6 = new THREE.BufferGeometry().setFromPoints(points6);
+            const controls = new OrbitControls(camera, renderer.domElement);
 
-    const line1 = new THREE.Line(geometry1, material);
-    const line2 = new THREE.Line(geometry2, material);
-    const line3 = new THREE.Line(geometry3, material);
-    const line4 = new THREE.Line(geometry4, material);
-    const line5 = new THREE.Line(geometry5, material);
-    const line6 = new THREE.Line(geometry6, material);
+            function animate() {
+                requestAnimationFrame(animate);
+                let time = clock.getElapsedTime();
+                scene.rotation.y = 3.14159 * Math.cos(time);  
+                controls.update();
+                renderer.render(scene, camera);
+            }
 
-    scene.add(line1, line2, line3, line4, line5, line6);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(scene, camera);
-    }
-    animate();
+            animate();
+        })
+        .catch(error => console.error('Error loading JSON data:', error));
 }
