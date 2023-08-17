77a16f40f0c0f2101a2dbbd95241f8c11c3130c9
nspeer12 <nspeer252@gmail.com>
2023-08-17 12:42:27 -0400

    update

--- src/nn.js
+++ src/nn.js
@@ -5,9 +5,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 function createSceneAndCamera() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 15; // Adjust camera position to fit neural network
+  camera.position.z = 12; // Adjust camera position to fit neural network
   camera.position.y = 5;
-  camera.position.x = 5;
+  camera.position.x = 4;
   return { scene, camera };
 }
 
